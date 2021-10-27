104ee4800e49232e385a8c9ee6dfe8eca76fdb33
SergRocket <rockettester3107@gmail.com>
2021-10-27 16:01:16 +0300

    Adding algo from work

--- src/test/java/AlgTraining.java
+++ src/test/java/AlgTraining.java
@@ -7,10 +7,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class AlgTraining {
@@ -54,6 +52,8 @@ public class AlgTraining {
             time = timeStop - timeStart;
         } while (time<timeToWait);
     }
+
+
 
     @Test
     public void score(){
@@ -354,5 +354,192 @@ public class AlgTraining {
     public void getSumOfValues(){
         sumOfValuesinArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4});
         Assert.assertEquals(6,sumOfValuesinArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    public static int sortDesc(final int num) {
+        int sortedNumber = 0;
+        for (int i = num; i >= 0; i--) {
+            int tmpNumber = num;
+            while (tmpNumber > 0) {
+                int digit = tmpNumber % 10;
+                // Check for the greatest digit in the given number
+                if (digit == i) {
+                    sortedNumber *= 10;
+                    sortedNumber += digit;
+                }
+                tmpNumber /= 10;
+            }
+        }
+        System.out.println(sortedNumber);
+        return sortedNumber;
+    }
+
+    @Test
+    public void sortNumbers() {
+        Assert.assertEquals(51, sortDesc(15));
+        Assert.assertEquals(4321, sortDe(1234));
+    }
+
+
+    public static int sortDe(final int num) {
+        return Integer.parseInt(String.valueOf(num)
+                .chars()
+                .mapToObj(i -> String.valueOf(Character.getNumericValue(i)))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.joining()));
+    }
+
+    public static boolean isIsogram(String str) {
+        // clever
+        //return str.length() == str.toLowerCase().chars().distinct().count();
+
+        return str.length() == 0 || str.length() ==
+                new HashSet<>(Arrays.asList(str.toLowerCase().split(""))).size();
+    }
+
+    @Test
+    public void checkIsogram() {
+        Assert.assertTrue(true, String.valueOf(isIsogram("Dermatoglyphics")));
+        Assert.assertTrue(true, String.valueOf(isIsogram("thumbscrewjapingly")));
+    }
+
+    public static boolean isIsog(String str) {
+        int i, j;
+        char[] symb = str.toCharArray();
+        Arrays.sort(symb);
+        str = str.toLowerCase();
+        for (i = 0; i < str.length(); ++i) {
+            for (j = i + 1; j < str.length(); ++j) {
+                if (symb[i] == symb[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    @Test
+    public void isIS(){
+        Assert.assertTrue(true, String.valueOf(isIsog("thumbscrewjapingly")));
+    }
+
+    public static int odds(int [] arrayNum){
+        List<Integer> odds = new ArrayList<>();
+        List<Integer> Noodds = new ArrayList<>();
+        StringBuilder st = new StringBuilder();
+        for(int i=0; i<arrayNum.length; i++){
+            if(arrayNum[i] %2 !=0){
+                odds.add(arrayNum[i]);
+            }
+        }
+        for(int i=0; i<arrayNum.length; i++){
+            if(arrayNum[i] %2 ==0){
+                Noodds.add(arrayNum[i]);
+            }
+        }
+        if(odds.size() > Noodds.size()){
+            int[] arr = Noodds.stream().mapToInt(i -> i).toArray();
+            for(int i : arr){
+                st.append(i);
+            }
+        } else if(odds.size() < Noodds.size()){
+            int[] arrs = odds.stream().mapToInt(i -> i).toArray();
+            for(int i : arrs){
+                st.append(i);
+            }
+
+        }
+        System.out.println(Integer.parseInt(st.toString()));
+        return Integer.parseInt(st.toString());
+    }
+
+    @Test
+    public void getOdd(){
+        odds(new int[]{34,12,7});
+        odds(new int[]{206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781});
+        int[] exampleTest1 = {2,6,8,-10,3};
+        int[] exampleTest2 = {206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781};
+        int[] exampleTest3 = {Integer.MAX_VALUE, 0, 1};
+        Assert.assertEquals(3, odds(new int[]{2,6,8,-10,3}));
+        Assert.assertEquals(206847684, odds(new int[]{206847684,1056521,7,17,1901,21104421,7,1,35521,1,7781}));
+        Assert.assertEquals(0, odds(exampleTest3));
+    }
+
+    public static int find_max(int nums[], int len) {
+        int i, max_num = nums[0];
+        for (i = 1; i < len; i++)
+            if (nums[i] > max_num) {
+                // (Fill in the missing line here)
+            }
+        return max_num;
+    }
+
+    @Test
+    public void testTask() {
+        find_max(new int[]{12, 13, 13}, 12);
+    }
+
+    @Test
+    public void scoreі() {
+        String[] ops = new String[]{"5", "2", "C", "D", "+"};
+        List<Integer> resu = new ArrayList<>();
+        String filter = ops[0];
+        String filter1 = ops[1];
+        String filter2 = ops[2];
+        String filter3 = ops[3];
+        String filter4 = ops[4];
+        Arrays.sort(ops);
+        for (String op : ops) {
+            if (op.startsWith(filter)) {
+                resu.add(Integer.valueOf(op));
+            } else if (op.startsWith(filter1)) {
+                resu.add(Integer.valueOf(op));
+            } else if (op.startsWith(filter2)) {
+                resu.remove(1);
+            } else if (op.startsWith(filter3)) {
+                resu.add(10);
+            } else if (op.startsWith(filter4)) {
+                resu.add(15);
+            }
+        }
+        System.out.print(resu);
+        Integer sum = resu.stream()
+                .reduce(0, (a, b) -> a + b);
+        System.out.print(sum);
+        String[] ops2 = new String[]{"5", "-2", "4", "C", "D", "9", "+", "+"};
+        List<Integer> resu2 = new ArrayList<>();
+        String filters = ops2[0];
+        String filters1 = ops2[1];
+        String filters2 = ops2[2];
+        String filters3 = ops2[3];
+        String filters4 = ops2[4];
+        String filters5 = ops2[5];
+        String filters6 = ops2[6];
+        Arrays.sort(ops2);
+        for (String opi : ops2) {
+            if (opi.startsWith(filters)) {
+                resu2.add(Integer.valueOf(opi));
+            } else if (opi.startsWith(filters1)) {
+                resu2.add(Integer.valueOf(opi));
+            } else if (opi.startsWith(filters2)) {
+                resu2.add(Integer.valueOf(opi));
+            } else if (opi.startsWith(filters3)) {
+                resu2.remove(2);
+            } else if (opi.startsWith(filters4)) {
+                resu2.add(-4);
+            } else if (opi.startsWith(filters5)) {
+                resu2.add(9);
+            } else if (opi.startsWith(filters6)) {
+                resu2.add(5);
+                resu2.add(14);
+            }
+        }
+        System.out.print(resu);
+        Integer sum2 = resu2.stream()
+                .reduce(0, (a, b) -> a + b);
+        System.out.print(sum2);
+        //String string = resu.stream().map(a->a.toString()).collect(Collectors.joining(", "));
+        //int recorded = Integer.parseInt(string);
+        // System.out.print(string);
     }
 }
